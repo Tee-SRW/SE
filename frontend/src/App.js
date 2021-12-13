fa5c15225ee00b93dcc246d193a5e2b20bb8f0cb
alptraumchariot1996 <83825308+alptraumchariot1996@users.noreply.github.com>
2021-12-13 16:39:14 +0700

    update 13/12/2021

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,64 @@
-import React , {useState}from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import ChooseCreate from './components/pages/ChooseCreate';
-import SignUp from './components/pages/SignUp';
-import SignUpcompany from './components/pages/SignUpcompany';
-import Login from './components/pages/Login';
-import Forgetpassword from './components/pages/Forgetpassword';
-import Resetpassword from './components/pages/Resetpassword';
-import Editprofile from './components/pages/Editprofile';
-import Profile from './components/pages/Profile';
-import EditprofileCompany from './components/pages/EditprofileCompany';
-import EditprofileFreelacne from './components/pages/EditprofileFreelance';
-import Freelance from './components/pages/Freelance';
-import CreateWorkFreeLance from './components/pages/CreateWorkFreeLance';
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import ChooseCreate from "./components/pages/ChooseCreate";
+import SignUp from "./components/pages/SignUp";
+import SignUpcompany from "./components/pages/SignUpcompany";
+import Login from "./components/pages/Login";
+import Forgetpassword from "./components/pages/Forgetpassword";
+import Resetpassword from "./components/pages/Resetpassword";
+import Editprofile from "./components/pages/Editprofile";
+import Profile from "./components/pages/Profile";
+import EditprofileCompany from "./components/pages/EditprofileCompany";
+import EditprofileFreelacne from "./components/pages/EditprofileFreelance";
+import Freelance from "./components/pages/Freelance";
+import CreateWorkFreeLance from "./components/pages/CreateWorkFreeLance";
 
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(
     //Test
-    false);
+    false
+  );
 
-    // initial value
-    //document.cookie.split(';').some((item) => item.trim().startsWith('logedIn=')));
+  // initial value
+  //document.cookie.split(';').some((item) => item.trim().startsWith('logedIn=')));
 
   return (
     <>
       <Router>
-        <Navbar {...{loggedIn}}/>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/ChooseCreate' component={ChooseCreate} />
-          <Route path='/Login' component={Login} />
-          <Route path='/sign-up' component={SignUp} />
-          <Route path='/sign-up-company' component={SignUpcompany} />
-          <Route path='/Forgetpassword' component={Forgetpassword} />
-          <Route path='/Resetpassword' component={Resetpassword} />
-          <Route path='/Editprofile' component={Editprofile} />
-          <Route path='/Profile' component={Profile} />
-          <Route path='/Editprofilefreelance' component={EditprofileFreelacne} />
-          <Route path='/EditprofileCompany' component={EditprofileCompany} />
-          <Route path='/Freelance' component={Freelance} />
-          <Route path='/Createworkfreelance' component={CreateWorkFreeLance} />
-        </Switch>
+        <Navbar {...{ loggedIn }} />
+        <div className="page__content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/services" component={Services} />
+            <Route path="/ChooseCreate" component={ChooseCreate} />
+            <Route path="/Login" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/sign-up-company" component={SignUpcompany} />
+            <Route path="/Forgetpassword" component={Forgetpassword} />
+            <Route path="/Resetpassword" component={Resetpassword} />
+            <Route path="/Editprofile" component={Editprofile} />
+            <Route path="/Profile" component={Profile} />
+            <Route
+              path="/Editprofilefreelance"
+              component={EditprofileFreelacne}
+            />
+            <Route path="/EditprofileCompany" component={EditprofileCompany} />
+            <Route path="/Freelance" component={Freelance} />
+            <Route
+              path="/Createworkfreelance"
+              component={CreateWorkFreeLance}
+            />
+          </Switch>
+        </div>
         <Footer />
       </Router>
-      
     </>
   );
 }
--- frontend/src/components/Profile/Profileform.js
+++ frontend/src/components/Profile/Profileform.js
@@ -8,19 +8,74 @@ import { Link } from "react-router-dom";
 import Image from "react-bootstrap/Image";
 import { Container, Row, Col } from "react-grid-system";
 export default function Profileform(props) {
+  let url = "";
+  const geturl = (e) => {
+    url = e.target.files[0].name;
+    console.log(url);
+  };
+  const [selectedImage, setSelectedImage] = useState();
+
+  // This function will be triggered when the file field change
+  const imageChange = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
+    }
+  };
+  const removeSelectedImage = () => {
+    setSelectedImage();
+  };
   return (
-    <Container className="Container-profile">
-      <div className="bgblock">
-        <div className="boardblock">
-          {/* <img
-          src="https://discountdoorhardware.ca/wp-content/uploads/2018/06/profile-placeholder-3.jpg"
-          className="rounded-circle image "
-          alt="Profile Admin"
-        ></img> */}
+    <Container className="container-profile">
+      <div className="bg">
+        <div className="board__container">
+          <Image
+            // src={URL.createObjectURL(selectedImage)} {selectedImage && ()}
+            // className="img-fluid rounded-circle image"
+            // alt="Profile Admin"
+            src="https://discountdoorhardware.ca/wp-content/uploads/2018/06/profile-placeholder-3.jpg"
+            className="img-fluid rounded-circle image"
+            alt="Profile Admin"
+            fluid
+          />
+          <div className="board__head">
+            <h1 className="board__name">ชื่ออะไรสักอย่าง</h1>
+            <div className="board__setting">
+              <div className="board__box">
+                {/* <inputvalue={url}
+                  className="d-none"
+                  type="file"
+                  multiple
+                  onChange={geturl}
+                  onChange={imageChange}
+                /> */}
+                <button className="btn btn-outline-primary bottom__profileform">
+                  แก้ไขโปรไฟล์
+                </button>
+              </div>
+            </div>
+          </div>
+          <h2 className="board__last">นามสกุลอะไรสักอย่าง</h2>
         </div>
-        <div className="Profileform-inner">
-          <label className="profilebox">ช่องทางการติดต่อ</label>
-          <text>เบอร์โทรศัพท์</text>
+        <h3>งานของฉัน</h3>
+        <div className="box_bg">
+          <div className="box__head__inner">
+            <label className="box__toptext">ช่องทางการติดต่อ</label>
+            <div>
+              <label className="box__midtext__start">
+                เบอร์โทรศัพท์
+                <label className="box__midtext__end">08x-xxx-xxxx</label>
+              </label>
+            </div>
+            <div>
+              <label className="box__bottomtext__start">
+                อีเมล
+                <label className="box__bottomtext__end">
+                  zzzzzz@hotmail.com
+                </label>
+              </label>
+            </div>
+          </div>
+          <div className="box__head__work"></div>
         </div>
       </div>
     </Container>
