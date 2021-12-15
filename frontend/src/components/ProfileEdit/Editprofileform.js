b8cda52110298380b5a0d49fc3ac15cbcf963d08
alptraumchariot1996 <83825308+alptraumchariot1996@users.noreply.github.com>
2021-12-15 14:06:01 +0700

    profile,editprofile

--- frontend/src/components/ProfileEdit/Editprofileform.js
+++ frontend/src/components/ProfileEdit/Editprofileform.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import "./Editprofileform.css";
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 import InputMask from "react-input-mask";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import Image from "react-bootstrap/Image";
 import { Container, Row, Col } from "react-grid-system";
-
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+const baseUsl = "http://203.170.190.226:8080/";
 export default function Editprofileform(props) {
+  const [values, setValues] = React.useState({
+    id: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    phone: "",
+    profile_user: "",
+  });
+  const handleValuesChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
   const [validated, setValidated] = useState(false);
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -16,56 +31,95 @@ export default function Editprofileform(props) {
       event.stopPropagation();
     }
     setValidated(true);
+    event.preventDefault();
+
+    const editprofile = {
+      id: values.id,
+      firstname: values.firstname,
+      lastname: values.lastname,
+      email: values.email,
+      password: values.password,
+      phone: values.phone,
+      profile_user: values.profile_user,
+    };
+    axios.put(`${baseUsl}/updateuser`, { editprofile }).then((res) => {
+      console.log(res.data);
+    });
+    if(form.checkValidity() === true) {
+      history.push("/Profile")
+    }
   };
+  const history = useHistory();
   return (
     <div className="Editprofileform-outer">
-    <div className="Editprofileform-inner">
-      <h3>โปรไฟล์ของฉัน</h3>
-      <div className="d-grid2">
-            <img
-              src=" "
-              className="img-fluid rounded-circle"
-              alt="Profile Admin"
-            ></img>
-          </div>
-          
-          <div className="d-grid2 gap-2 spacing-top btn-fontblack">
-            <button
-              type="submit"
-              className="btn btn-lg color bottomprofileprofile"
-            >
-              แก้ไขรูปโปรไฟล์
-            </button>
-            </div>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="4" controlId="validationCustom01">
-            <Form.Label>ชื่อ</Form.Label>
-            <Form.Control required type="text" placeholder="ชื่อ" />
-            <Form.Control.Feedback type="invalid">
-              กรุณาใส่ ชื่อ
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="4" controlId="validationCustom02">
-            <Form.Label>นามสกุล</Form.Label>
-            <Form.Control required type="text" placeholder="นามสกุล" />
-            <Form.Control.Feedback type="invalid">
-              กรุณาใส่ นามสกุล
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-            <Form.Label>อีเมล</Form.Label>
-            <Form.Control required type="text" placeholder="อีเมล" />
-            <Form.Control.Feedback type="invalid">
-              กรุณาใส่ อีเมล
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
+      <div className="Editprofileform-inner">
+        <h3>โปรไฟล์ของฉัน</h3>
+        <div className="d-grid2">
+          <img
+            src=" "
+            className="img-fluid rounded-circle"
+            alt="Profile Admin"
+          ></img>
+        </div>
+
+        <div className="d-grid2 gap-2 spacing-top btn-fontblack">
+          <button
+            type="submit"
+            className="btn btn-lg color bottomprofileprofile"
+          >
+            แก้ไขรูปโปรไฟล์
+          </button>
+        </div>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustom01">
+              <Form.Label>ชื่อ</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="ชื่อ"
+                name="firstname"
+                onChange={handleValuesChange("firstname")}
+                value={values.firstname}
+              />
+              <Form.Control.Feedback type="invalid">
+                กรุณาใส่ ชื่อ
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustom02">
+              <Form.Label>นามสกุล</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="นามสกุล"
+                name="lastname"
+                onChange={handleValuesChange("lastname")}
+                value={values.lastname}
+              />
+              <Form.Control.Feedback type="invalid">
+                กรุณาใส่ นามสกุล
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+              <Form.Label>อีเมล</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="อีเมล"
+                name="email"
+                onChange={handleValuesChange("email")}
+                value={values.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                กรุณาใส่ อีเมล
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
             <Form.Group as={Col} md="3" controlId="validationCustom05">
               <Form.Label>เบอร์โทรศัพท์</Form.Label>
               <InputMask
@@ -73,24 +127,31 @@ export default function Editprofileform(props) {
                 className="form-control spacing-top"
                 placeholder="xxx-xxx-xxxx"
                 mask="999-999-9999"
-                onChange={props.onChange}
-                value={props.value}
+                name="phone"
+                onChange={handleValuesChange("phone")}
+                value={values.phone}
               />
               <Form.Control.Feedback type="invalid">
                 กรุณาใส่ เบอร์โทรศัพท์
               </Form.Control.Feedback>
             </Form.Group>
           </Row>
-        <div className="d-grid gap-2 btn-color">
-          <Button
-            type="submit"
-            className="btn btn-lg color spacing-top10"
-          >
-            บันทึก
-          </Button>
-        </div>
-      </Form>
+          <div className="d-grid gap-2 btn-color">
+            <Button type="submit" className="btn btn-lg color spacing-top10 ">
+              บันทึก
+            </Button>
+          </div>
+          <div className="d-grid gap-2 btn-color">
+            <Button
+              type="submit"
+              className="btn btn-lg color spacing-top10 "
+              onClick={() => history.push("/Profile")}
+            >
+              ย้อนกลับ
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
-  </div>
   );
 }
