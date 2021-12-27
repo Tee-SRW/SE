38bf7d66f6ee7ed3ba092745ed64a8ac8a3adfd2
alptraumchariot1996 <83825308+alptraumchariot1996@users.noreply.github.com>
2021-12-27 22:40:45 +0700

    fix Profile

--- frontend/src/components/Profile/Profileform.js
+++ frontend/src/components/Profile/Profileform.js
@@ -5,7 +5,6 @@ import Image from "react-bootstrap/Image";
 import { Container, Row, Col } from "react-grid-system";
 // import axios from '../api/axios-profile';
 
-
 export default function Profileform(props) {
   let url = "";
   const geturl = (e) => {
@@ -42,32 +41,42 @@ export default function Profileform(props) {
     <Container className="container-profile">
       <div className="bg">
         <div className="board__container">
-          <Row className="mnarginl">
-            <Col>
-              <Image
-                // src={URL.createObjectURL(selectedImage)} {selectedImage && ()}
-                // className="img-fluid rounded-circle image"
-                // alt="Profile Admin"
-                src="/images/ProfileCEO.jpg"
-                className="img-fluid rounded-circle image"
-                // alt="Profile Admin"
-                valuesProfile={valuesProfile.Profile__image}
-                fluid
-              />
-            </Col>
-            <Col className="margin">
-              <h1 >สุชัย อัศะ</h1>
-              <button
-                className="btn btn-outline-primary bottom__profileform"
-                type="submit"
-                onClick={() => history.push("/Editprofile")}
-              >
-                แก้ไขโปรไฟล์
-              </button>
-            </Col>
-
-          </Row>
-
+          <Image
+            // src={URL.createObjectURL(selectedImage)} {selectedImage && ()}
+            // className="img-fluid rounded-circle image"
+            // alt="Profile Admin"
+            src="/images/ProfileCEO.jpg"
+            className="img-fluid rounded-circle image"
+            // alt="Profile Admin"
+            valuesProfile={valuesProfile.Profile__image}
+            fluid
+          />
+          <div className="board__head">
+            <h1 className="board__name">
+              สุชัย<text>{props.First__name__freelance}</text>
+            </h1>
+            <div className="board__setting">
+              <div className="board__box">
+                {/* <inputvalue={url}
+                  className="d-none"
+                  type="file"
+                  multiple
+                  onChange={geturl}
+                  onChange={imageChange}
+                /> */}
+                <button
+                  className="btn btn-outline-primary bottom__profileform"
+                  type="submit"
+                  onClick={() => history.push("/Editprofilefreelance")}
+                >
+                  แก้ไขโปรไฟล์
+                </button>
+              </div>
+            </div>
+          </div>
+          <h2 className="board__last__freelance">
+            อัศะ<text>{props.Last__name__freelance}</text>
+          </h2>
         </div>
 
         <div className="box_bg">
